16404b5807e867702de78e109f8c88b94dc5b177
Tape01 <salehjames01@gmail.com>
2023-10-28 00:19:28 +0100

    first commit

---
+++ blog/schemas.py
@@ -0,0 +1,48 @@
+from typing import List, Optional
+from pydantic import BaseModel
+
+
+class BlogBase(BaseModel):
+    title: str
+    body: str
+
+
+class Blog(BlogBase):
+    class Config():
+        from_attributes = True
+
+class User(BaseModel):
+    name: str
+    email: str
+    password: str
+
+
+class ShowUser(BaseModel):
+    name: str
+    email: str
+    blogs: List[Blog] = []
+    class Config():
+        from_attributes = True
+
+
+class ShowBlog(BaseModel):
+    title: str
+    body: str
+    creator: ShowUser
+
+    class Config():
+        from_attributes = True
+
+
+class Login(BaseModel):
+    username: str
+    password: str
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    email: Optional[str]= None
