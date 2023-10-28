db87705fd99fd5e33ba546bd7e2a2a4e68f48bf6
Tape01 <salehjames01@gmail.com>
2023-10-28 01:26:17 +0100

    new commit

--- blog/main.py
+++ blog/main.py
@@ -13,5 +13,5 @@ app.include_router(user.router)
 
 if __name__ == "__main__":
     import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
 
-    uvicorn.run(app, host="0.0.0.0", port=8000)
