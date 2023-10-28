fa5c5cba232ee77114ca58b427dfa227422f03d0
Tape01 <salehjames01@gmail.com>
2023-10-28 02:24:39 +0100

    add

--- blog/main.py
+++ blog/main.py
@@ -11,6 +11,6 @@ app.include_router(authentication.router)
 app.include_router(blog.router)
 app.include_router(user.router)
 
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
+# if __name__ == "__main__":
+#     uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
 
