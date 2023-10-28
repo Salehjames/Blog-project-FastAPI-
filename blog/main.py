5e29a98efe01db65532a96497fef1789abe5069b
Tape01 <salehjames01@gmail.com>
2023-10-28 01:12:55 +0100

    new changes

--- blog/main.py
+++ blog/main.py
@@ -11,10 +11,7 @@ app.include_router(authentication.router)
 app.include_router(blog.router)
 app.include_router(user.router)
 
+if __name__ == "__main__":
+    import uvicorn
 
-
-
-
-
-
-
+    uvicorn.run(app, host="0.0.0.0", port=8000)
