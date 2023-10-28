6e38849a2680d068ee414df22432043af9a6db27
Tape01 <salehjames01@gmail.com>
2023-10-28 08:06:35 +0100

    added

---
+++ app/blog/main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+from blog import models
+from blog.database import engine
+from blog.routers import blog, user, authentication
+
+
+app = FastAPI()
+
+models.Base.metadata.create_all(engine)
+app.include_router(authentication.router)
+app.include_router(blog.router)
+app.include_router(user.router)
+
+# if __name__ == "__main__":
+#     uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
+
---
+++ app/blog/routers/user.py
@@ -0,0 +1,21 @@
+from fastapi import APIRouter
+from blog import database, schemas
+from sqlalchemy.orm import Session
+from fastapi import APIRouter, Depends
+from blog.repository import user
+
+
+router = APIRouter(
+    prefix="/user",
+    tags=['User']
+)
+get_db =database.get_db
+
+
+@router.post('/', response_model=schemas.ShowUser)
+def create_user(request: schemas.User,db : Session = Depends(get_db)):
+    return user.create(request, db)
+
+@router.get('/{id}', response_model=schemas.ShowUser)
+def get_user(id:int, db: Session = Depends(get_db)):
+    return user.show(id, db)
